137aa63243972f7b558bedf57b07596135a85279
kimHyeonJu <kimhyesom1202@gmail.com>
2025-01-06 22:46:32 +0900

    ✨ Basic of component in React
    Chapter 03-1. 컴포넌트란?

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-/* 
-  ※ JSX로 구구단 출력하기
-  - 구구단 2~9단을 출력하세요.
-  - 단, 5단은 제외합니다.
-  - 홀수 단은 다른 색으로 표시합니다.
-  - 구구단은 오른쪽으로 나열되도록 합니다.
-*/
-
-const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+import Hello from "./Hello.js";
+import World from "./World";
 
 export default function App() {
   return (
     <div>
-      <div style={{ display: "flex" }}>
-        {/* 여기에 JSX를 활용하여 요구사항에 맞는 구구단을 완성시켜보세요! */}
-        {num.map(
-          (n) =>
-            // if (n === 1 || n === 5) {
-            //   return null;
-            // }
-            n >= 2 &&
-            n !== 5 && (
-              <div style={{ padding: 10 }}>
-                {num.map((m) => {
-                  return (
-                    <div style={{ color: n % 2 === 1 ? "blue" : "black" }}>
-                      {n} x {m} = {n * m}
-                    </div>
-                  );
-                })}
-              </div>
-            ),
-        )}
-      </div>
+      <Hello></Hello>
+      <World />
     </div>
   );
 }
