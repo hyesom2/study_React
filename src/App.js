ac1c69218b35f89e24850b25503ec747e767091e
kimHyeonJu <kimhyesom1202@gmail.com>
2025-01-06 19:47:15 +0900

    ✨ Conditional statement of JSX in React
    Chapter 02-3. JSX에서의 조건문

--- src/App.js
+++ src/App.js
@@ -1,50 +1,29 @@
-const text = "Hello, World!";
-const num = 10;
-const obj = { key: 0, a: 1, b: 2 };
-const arr = ["a", "b", "c"];
-const imageUrl =
-  "https://cdn.pixabay.com/photo/2023/10/23/16/24/bird-8336436_1280.jpg";
+const arr = [1, 2, 3];
+const arr1 = [];
+const text = "";
 
 export default function App() {
   return (
     <div>
-      <h1>변수 넣기</h1>
+      <h1>삼항연산자</h1>
       <ol>
-        <li>{text}</li>
-        <li>{text + "test"}</li>
+        <li>{1 + 1 === 2 ? "참입니다." : "거짓입니다."}</li>
+        <li>{1 + 1 === 3 ? "참입니다." : "거짓입니다."}</li>
       </ol>
 
-      <h1>숫자 및 계산식 넣기</h1>
+      <h1>AND 연산자</h1>
       <ol>
-        <li>{num}</li>
-        <li>{num + 15}</li>
+        <li>{1 + 1 === 2 && "AND 연산자1"}</li>
+        <li>{1 + 1 !== 2 && "AND 연산자1"}</li>
+        <li>{arr.length && "AND 연산자2"}</li>
+        <li>{arr1.length && "AND 연산자2"}</li>
+        <li>{!!arr1.length && "AND 연산자2"}</li>
       </ol>
 
-      <h1>Boolean, Nullish 값 넣기</h1>
+      <h1>OR 연산자</h1>
       <ol>
-        <li>{true}</li>
-        <li>{false}</li>
-        <li>{undefined}</li>
-        <li>{null}</li>
-      </ol>
-
-      <h1>Object, Array 넣기</h1>
-      <ol>
-        {/* <li>{obj}</li> */}
-        <li>{arr}</li>
-        <li>{[<div>111</div>, <div>222</div>, <div>333</div>]}</li>
-      </ol>
-
-      <h1>주석 넣기</h1>
-      <ol>
-        <li>{/* 주석 넣기 */}</li>
-      </ol>
-
-      <h1>태그 속성에 넣기</h1>
-      <ol>
-        <li>
-          <img src={imageUrl} alt="새" style={{ width: 400 }} />
-        </li>
+        <li>{1 + 1 !== 2 || "OR 연산자1"}</li>
+        <li>{text || "OR 연산자2"}</li>
       </ol>
     </div>
   );
