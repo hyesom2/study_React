d7ee51f7a1364df8aa45e0cb00253ed7aecb2085
kimHyeonJu <kimhyesom1202@gmail.com>
2025-01-06 23:06:15 +0900

    ✨ Basic of props in React
    Chapter 03-2. Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import Hello from "./Hello.js";
-import World from "./World";
+import Heading from "./Heading";
 
 export default function App() {
   return (
     <div>
-      <Hello></Hello>
-      <World />
+      <MyComponent value={"test"} />
+      <MyComponent2>MyComponent2의 children</MyComponent2>
+      <Heading type="h1">Hello,</Heading>
+      <Heading type="h2">World!</Heading>
     </div>
   );
+}
+
+function MyComponent(props) {
+  return <h1>{props.value}</h1>;
+}
+
+function MyComponent2(props) {
+  return <h1>{props.children}</h1>;
 }
