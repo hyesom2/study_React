405939b2b1dba9a6a73ab8cc36cde909343fadce
kimHyeonJu <kimhyesom1202@gmail.com>
2025-01-06 21:46:16 +0900

    ✨ Styling JSX in React
    Chapter 02-5. JSX 스타일링

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
-const arr = ["1번", "2번", "3번", "3번"];
-const arr2 = [];
-for (let i = 0; i < arr.length; i++) {
-  arr2.push(<h4 key={i}>{arr[i]}</h4>);
-  // = [<h4>1번</h4>, <h4>2번</h4>, <h4>3번</h4>, <h4>3번</h4>]
-}
+import "./index.css";
+
+// 2. style 재활용
+const boxStyle = {
+  position: "relative",
+  top: 50,
+  left: 50,
+  width: "50%",
+  height: "200px",
+  padding: 20,
+  background: "#ffaaaa",
+  // 3. 속성은 camelCase
+  borderRadius: 50,
+};
 
 export default function App() {
   return (
-    <div>
-      <h1>배열로 넣기</h1>
-      <ol>
-        <li>{arr}</li>
-        <li>{arr2}</li>
-      </ol>
+    <div
+      style={{
+        // 1. object로 css 작성
+        position: "relative",
+        width: 400,
+        height: 1000,
+        background: "#eee",
+      }}
+    >
+      <div style={boxStyle}>Hello1</div>
 
-      <hr />
+      <div style={{ ...boxStyle, top: 100 }}>
+        {/* 4. className을 통한 스타일링 (css-in-JS) */}
+        <div className="highlight">Hello2</div>
+      </div>
 
-      <h1>Array.map</h1>
-      <ol>
-        <li>
-          {arr.map((item, index) => {
-            return <h4 key={`${item}-${index}`}>{item}</h4>;
-          })}
-        </li>
-      </ol>
+      <div style={{ ...boxStyle, top: 150 }}>
+        {/* 5. 조건적 스타일 */}
+        <div
+          className={
+            1 + 1 === 2 ? "highlight" : ""
+            // 위와 아래의 결과는 동일하다.
+            // 1 + 1 === 2 && "highlight"
+          }
+        >
+          Hello3
+        </div>
+      </div>
     </div>
   );
 }
