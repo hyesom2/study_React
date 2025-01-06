1af2657b4a60b6be7c4e40c9d2495ab4a5cb70cc
kimHyeonJu <kimhyesom1202@gmail.com>
2025-01-06 22:29:13 +0900

    ✨ Print multiplication table with JSX in React
    Chapter 02-6. JSX로 구구단 출력하기

--- src/App.js
+++ src/App.js
@@ -1,47 +1,36 @@
-import "./index.css";
+/* 
+  ※ JSX로 구구단 출력하기
+  - 구구단 2~9단을 출력하세요.
+  - 단, 5단은 제외합니다.
+  - 홀수 단은 다른 색으로 표시합니다.
+  - 구구단은 오른쪽으로 나열되도록 합니다.
+*/
 
-// 2. style 재활용
-const boxStyle = {
-  position: "relative",
-  top: 50,
-  left: 50,
-  width: "50%",
-  height: "200px",
-  padding: 20,
-  background: "#ffaaaa",
-  // 3. 속성은 camelCase
-  borderRadius: 50,
-};
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function App() {
   return (
-    <div
-      style={{
-        // 1. object로 css 작성
-        position: "relative",
-        width: 400,
-        height: 1000,
-        background: "#eee",
-      }}
-    >
-      <div style={boxStyle}>Hello1</div>
-
-      <div style={{ ...boxStyle, top: 100 }}>
-        {/* 4. className을 통한 스타일링 (css-in-JS) */}
-        <div className="highlight">Hello2</div>
-      </div>
-
-      <div style={{ ...boxStyle, top: 150 }}>
-        {/* 5. 조건적 스타일 */}
-        <div
-          className={
-            1 + 1 === 2 ? "highlight" : ""
-            // 위와 아래의 결과는 동일하다.
-            // 1 + 1 === 2 && "highlight"
-          }
-        >
-          Hello3
-        </div>
+    <div>
+      <div style={{ display: "flex" }}>
+        {/* 여기에 JSX를 활용하여 요구사항에 맞는 구구단을 완성시켜보세요! */}
+        {num.map(
+          (n) =>
+            // if (n === 1 || n === 5) {
+            //   return null;
+            // }
+            n >= 2 &&
+            n !== 5 && (
+              <div style={{ padding: 10 }}>
+                {num.map((m) => {
+                  return (
+                    <div style={{ color: n % 2 === 1 ? "blue" : "black" }}>
+                      {n} x {m} = {n * m}
+                    </div>
+                  );
+                })}
+              </div>
+            ),
+        )}
       </div>
     </div>
   );
