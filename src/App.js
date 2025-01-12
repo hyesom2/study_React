edcef4eb9d648b1b865959cb4c6ffebc455f2cb6
kimHyeonJu <kimhyesom1202@gmail.com>
2025-01-12 21:48:11 +0900

    ✨ Saving data with localStorage
    Chapter 07-6. localStorage로 데이터 보관하기

--- src/App.js
+++ src/App.js
@@ -1,40 +1,68 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import MemoContainer from "./components/MemoContainer";
+import { setItem, getItem } from "./lib/Storage";
+import debounce from "lodash.debounce";
+
+const debounceSetItem = debounce(setItem, 5000);
 
 export default function App() {
-  const [memos, setMemos] = useState([
-    {
-      title: "memo1",
-      content: "this is memo1",
-      createdTime: "1736305630047", // 생성된 시간 new Date().getTime();
-      updatedTime: "1736305630047", // 변경된 시간
-    },
-    {
-      title: "memo2",
-      content: "this is memo2",
-      createdTime: "1836305630047", // 생성된 시간 new Date().getTime();
-      updatedTime: "1836305630047", // 변경된 시간
-    },
-  ]);
-
+  // const [memos, setMemos] = useState([
+  //   {
+  //     title: "memo1",
+  //     content: "this is memo1",
+  //     createdTime: "1736305630047", // 생성된 시간 new Date().getTime();
+  //     updatedTime: "1736305630047", // 변경된 시간
+  //   },
+  //   {
+  //     title: "memo2",
+  //     content: "this is memo2",
+  //     createdTime: "1836305630047",
+  //     updatedTime: "1836305630047",
+  //   },
+  // ]);
+  const [memos, setMemos] = useState(
+    // JSON.parse(localStorage.getItem("memos")) || [],
+    getItem("memos") || [],
+  );
   const [selectedMemoIndex, setSelectedMemoIndex] = useState(0);
 
+  /*
   function setMemo(newMemo) {
-    // memos[selectedMemoIndex] = newMemo; // memos[0]이 newMemo로 교체된다.
-    // console.log("memos", memos);
-    // setMemos([...memos]);
-
     const newMemos = [...memos];
     newMemos[selectedMemoIndex] = newMemo;
     setMemos(newMemos);
+    // localStorage.setItem("memos", JSON.stringify(newMemos));
+    // setItem("memos", newMemos);
+    debounceSetItem("memos", newMemos);
   }
+  */
+  const setMemo = useCallback(
+    (newMemo) => {
+      // const newMemos = [...memos];
 
-  function addMemo() {
+      // newMemos[selectedMemoIndex] = newMemo;
+
+      // setMemos(newMemos);
+      setMemos((memos) => {
+        const newMemos = [...memos];
+
+        newMemos[selectedMemoIndex] = newMemo;
+        debounceSetItem("memos", newMemos);
+
+        return newMemos;
+      });
+      // localStorage.setItem("memos", JSON.stringify(newMemos));
+      // setItem("memos", newMemos);
+      // debounceSetItem("memos", newMemos);
+    },
+    [selectedMemoIndex],
+  );
+
+  const addMemo = useCallback(() => {
     const time = new Date().getTime();
-
-    setMemos([
+    const newMemos = [
       ...memos,
       {
         title: "Untitled",
@@ -42,19 +70,33 @@ export default function App() {
         createdTime: time,
         updatedTime: time,
       },
-    ]);
-    setSelectedMemoIndex(memos.length);
-  }
-
-  function deleteMemo(index) {
-    const newMemos = [...memos];
-    newMemos.splice(index, 1);
+    ];
 
     setMemos(newMemos);
-    if (index === selectedMemoIndex) {
-      setSelectedMemoIndex(index - 1);
-    }
-  }
+    setSelectedMemoIndex(memos.length);
+    debounceSetItem("memos", newMemos);
+  }, [memos]);
+
+  const deleteMemo = useCallback(
+    (index) => {
+      // const newMemos = [...memos];
+      // newMemos.splice(index, 1);
+
+      // setMemos(newMemos);
+      setMemos((memos) => {
+        const newMemos = [...memos];
+
+        newMemos.splice(index, 1);
+        debounceSetItem("memos", newMemos);
+
+        return newMemos;
+      });
+      if (index === selectedMemoIndex) {
+        setSelectedMemoIndex(index - 1);
+      }
+    },
+    [selectedMemoIndex],
+  );
 
   return (
     <div className="App">
